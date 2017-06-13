2437e5cd44f86f2ac64cc8ab1702cb3afa8520d4
Fernando Villalobos <fernando.visa@gmail.com>
2017-06-12 19:22:53 -0500

    Make current_user available to main app's views

--- app/controllers/cerberus/authentication.rb
+++ app/controllers/cerberus/authentication.rb
@@ -4,6 +4,10 @@ module Cerberus
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
+    def self.included m
+      m.helper_method :current_user
+    end
+
   private
 
     def authenticate!
